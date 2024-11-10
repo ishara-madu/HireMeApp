6384aa5fc9c058d49b1c4fa2c97a19a4985766eb
Ishara <isharamadushankab@gmail.com>
2024-11-10 15:16:33 +0530

    update App.tsx and SignUp.tsx

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import SignIn from './src/components/SignIn';
-import SignUp from './src/components/SignUp';
+import Auth from './src/pages/Auth';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { ThemeProvider } from './src/context/ThemeContext';
+import SignUp from './src/components/SignUp';
 
 
 export default function App() {
   return (
     <SafeAreaView className='flex-1 '>
-        {/* <SignIn/> */}
-        <SignUp />
+        {/* <SignUp/> */}
+        <Auth />
       </SafeAreaView>
   );
 }
