9717c7131e9b5a57076549aff618e1cbd6595101
Ishara <isharamadushankab@gmail.com>
2024-11-12 18:52:25 +0530

    update profile.png, Employees.tsx and NearbyYou.tsx

--- src/components/home/Employees.tsx
+++ src/components/home/Employees.tsx
@@ -1,14 +1,12 @@
 
 import { View, Text } from 'react-native'
 import React from 'react'
+import NearbyYou from './employees/NearbyYou'
 
 const Employees = () => {
     return (
-        <View className={`w-11/12 mt-3`}>
-            <Text className={`text-xs font-semibold`}>Nearby You</Text>
-            <View className={`w-full`}>
-
-            </View>
+        <View className={`w-11/12 mt-5`}>
+            <NearbyYou/>
         </View>
     )
 }
